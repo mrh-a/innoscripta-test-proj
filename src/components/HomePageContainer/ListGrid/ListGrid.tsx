49a3af9db38877f3ca9cc89df256e0b25fafcdfb
Hamid <mralijani.h@gmail.com>
2025-02-09 16:03:29 +0330

    Home page component with Search Bar and ListGrid

---
+++ src/components/HomePageContainer/ListGrid/ListGrid.tsx
@@ -0,0 +1,75 @@
+import { FC, useEffect } from "react";
+import { useLocation, useParams } from "react-router"
+import useSearchQueryParams from "../../../core/hooks/use-search-query-params.hook";
+import { useNewsAPIGetTopHeadlines } from "../../../core/api/news-api.api";
+import ArticleCard from "../../common/ArticleCard/ArticleCard";
+import { IOption } from "../../../core/model/option.model";
+import Pagination from "../../common/Pagination/Pagination";
+
+const sourcesOption: IOption[] = [];
+
+interface IListGrid {}
+
+const ListGrid: FC<IListGrid> = () => {
+
+  const location = useLocation();
+  const { dataSource, search, dateRange, category, sourceId } =
+    useSearchQueryParams(location.search, sourcesOption);
+
+  const { page: pageParam } = useParams<{ page: string }>();
+  const currentPage = pageParam ? pageParam : "1";
+
+  const { data, isLoading, refetch } = useNewsAPIGetTopHeadlines({
+    category,
+    dataSource,
+    dateRange,
+    search,
+    sourceId,
+    page: currentPage,
+    pageSize: "10",
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [dataSource, search, dateRange, category, sourceId, refetch]);
+
+
+  let totalPages = 0;
+  
+  if(data && data.data.totalResults){
+    totalPages = data.data.totalResults;
+  }
+
+  const onPageChange = (selected: number) => {
+    console.log("--selected--", selected);
+  }
+
+  return (
+    <section>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-[20px]">
+        {data?.data.articles.map((article) => (
+          <ArticleCard
+            description={article.description}
+            href={article.url}
+            image={article.urlToImage}
+            title={article.title}
+            key={article.url}
+            author={article.author}
+            date={article.publishedAt}
+            source={article.source.name}
+            category={"politics"}
+          />
+        ))}
+      </div>
+
+      <Pagination
+        page={+currentPage}
+        pageSize={10}
+        total={totalPages}
+        onPageChange={onPageChange}
+      />
+    </section>
+  );
+};
+
+export default ListGrid;
