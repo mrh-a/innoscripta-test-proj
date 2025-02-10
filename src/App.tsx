b7dd47ef7a3f04b638e88485ced0b1c77d68069b
Hamid <mralijani.h@gmail.com>
2025-02-10 11:18:34 +0330

    add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import { Route, Routes } from 'react-router';
-import { MainRoutes } from './core/config/routes.config';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Route, Routes } from "react-router";
+import { MainRoutes } from "./core/config/routes.config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Header from "./components/Header/Header";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
-
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
   return (
     <div className="">
       <ToastContainer />
-      <div className="container mx-auto h-[2000px] py-[10px] shadow">
+      <div className="container mx-auto py-[10px] shadow">
         <QueryClientProvider client={queryClient}>
           <Header />
-          <main className="p-[20px]">
-            <Routes>
-              {MainRoutes.map(({ component: Component, route }) => (
-                <Route key={route} path={route} element={<Component />} />
-              ))}
-            </Routes>
-          </main>
+          <ErrorBoundary
+            fallback={
+              <div className="text-[24px] text-center font-bold mt-[300px]">
+                Something went wrong!
+              </div>
+            }
+          >
+            <main className="p-[20px]">
+              <Routes>
+                {MainRoutes.map(({ component: Component, route }) => (
+                  <Route key={route} path={route} element={<Component />} />
+                ))}
+              </Routes>
+            </main>
+          </ErrorBoundary>
           <footer></footer>
         </QueryClientProvider>
       </div>
@@ -28,4 +36,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
